c367abd4835c77cebabf0f63f046e5d6a57e866d
Nonos1500 <erdeneuv@duck.com>
2024-02-14 16:46:29 -0600

    Initial commit: Added project files

---
+++ src/test/java/com/sample/pages/BasePage.java
@@ -0,0 +1,4 @@
+package com.sample.pages;
+
+public abstract class BasePage {
+}
---
+++ src/test/java/com/sample/stepdefinitions/Hooks.java
@@ -0,0 +1,45 @@
+package com.sample.stepdefinitions;
+
+import com.sample.utilities.ConfigurationReader;
+import com.sample.utilities.DB_Util;
+import com.sample.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.time.Duration;
+
+public class Hooks {
+
+    @Before("@ui")
+    public void setUp(){
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
+
+    }
+
+    @After("@ui")
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
+    }
+
+    @Before("@db")
+    public void setUpDB(){
+        System.out.println("Connecting to database...");
+        DB_Util.createConnection();
+    }
+
+    @After("@db")
+    public void tearDownDB(){
+        System.out.println("close database connection...");
+        DB_Util.destroy();
+    }
+}
