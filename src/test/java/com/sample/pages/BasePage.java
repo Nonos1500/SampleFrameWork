19e7208ef126764712f3457ab3b4aef0a1e1639d
Nonos1500 <erdeneuv@duck.com>
2024-02-16 12:42:52 -0600

    swith module method created

--- src/test/java/com/sample/pages/BasePage.java
+++ src/test/java/com/sample/pages/BasePage.java
@@ -1,4 +1,22 @@
 package com.sample.pages;
 
+import com.sample.utilities.Driver;
+import org.openqa.selenium.By;
+
 public abstract class BasePage {
+
+    public void navigateModule(String moduleName){
+        switch (moduleName){
+            case "rent":
+                moduleName = "Rent Menu";
+                break;
+            case "buy":
+                moduleName = "Buy Menu";
+                break;
+        }
+
+        Driver.getDriver().findElement(By.xpath(
+                "//a[@title='"+moduleName+"']"
+        )).click();
+    }
 }
