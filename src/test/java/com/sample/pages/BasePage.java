fcde849336cad992f0efd6cd6dbbb5702c138330
nonos1500 <erdeneuv@duck.com>
2024-02-16 11:27:46 -0600

    Redfin search feature and stepDef created

--- src/test/java/com/sample/pages/BasePage.java
+++ src/test/java/com/sample/pages/BasePage.java
@@ -1,4 +1,23 @@
 package com.sample.pages;
 
+import com.sample.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public abstract class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "search-box-input")
+    public WebElement searchInput;
+
+    @FindBy (xpath = "//button[@type = 'submit']")
+    public WebElement searchBtn;
+
+    @FindBy (xpath = "//h1[@class='collapsedListView']")
+    public WebElement searchTitle;
+
 }
